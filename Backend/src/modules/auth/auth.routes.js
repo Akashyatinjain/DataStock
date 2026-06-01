35b5f9489409963c14644c86ecbfb5c990eae96f
Akashjain <aj0881871@gmail.com>
2026-06-02 01:40:38 +0530

    adding auth files 3.4

--- Backend/src/modules/auth/auth.routes.js
+++ Backend/src/modules/auth/auth.routes.js
@@ -2,6 +2,8 @@ import express from "express"
 import { signInUser, signUpUser,logoutUser } from "./auth.controller.js"
 import { signUpvalidation,loginValidation } from "./auth.validation.js";
 import passport from "./providers/googleAuth.js";
+import jwt from "jsonwebtoken";
+
 import {
   sendOTPController,
   verifyOTPController
@@ -24,10 +26,17 @@ router.get(
   "/google/callback",
   passport.authenticate("google", { session: false, failureRedirect: "/login" }),
   (req, res) => {
-    res.redirect(`${process.env.VITE_API_URL}/dashboard`);
+    // Generate JWT from the user passport gave you
+    const token = jwt.sign(
+      { id: req.user.googleId, email: req.user.email, name: req.user.name },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    // Send token to frontend via URL param
+    res.redirect(`${process.env.VITE_API_URL}/dashboard?token=${token}`);
   }
 );
-
 
 router.post("/send-otp", sendOTPController);
 router.post("/verify-otp", verifyOTPController);

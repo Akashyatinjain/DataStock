650639ffee3eb8decf2a646ea8ea79e74fa81371
Akashjain <aj0881871@gmail.com>
2026-06-02 01:14:23 +0530

    adding the url

--- Backend/src/modules/auth/providers/googleAuth.js
+++ Backend/src/modules/auth/providers/googleAuth.js
@@ -38,7 +38,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:  `${import.meta.env.VITE_API_URL}/auth/google/callback`,
+      callbackURL: `${process.env.BACKEND_URL}auth/google/callback`
     },
     async (accessToken, refreshToken, profile, done) => {
       const email =

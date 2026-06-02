dc8946fc010a9266da2f76e5a6d845f355a5339d
Akashjain <aj0881871@gmail.com>
2026-06-02 14:04:55 +0530

    adding the resend instend of gmail sender

--- Backend/src/utils/email.util.js
+++ Backend/src/utils/email.util.js
@@ -8,22 +8,27 @@ import nodemailer  from "nodemailer";
 //     pass: process.env.EMAIL_PASS,
 //   },
 // });
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-transporter.verify((error, success) => {
-  if (error) {
-    console.log("SMTP ERROR:", error);
-  } else {
-    console.log("SMTP READY");
-  }
-});
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendOTPEmail = async (email, otp) => {
+  const response = await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "OTP Verification",
+    text: `Your OTP is ${otp}`,
+  });
+
+  console.log(response);
+};
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.log("SMTP ERROR:", error);
+//   } else {
+//     console.log("SMTP READY");
+//   }
+// });
 // export const sendOTPEmail = async (email, otp) => {
 //   await transporter.sendMail({
 //     from: process.env.EMAIL_USER,
@@ -33,18 +38,18 @@ transporter.verify((error, success) => {
 //   });
 // };
 
-export const sendOTPEmail = async (email, otp) => {
-  try {
-    const info = await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject: "Your OTP Verification Code",
-      text: `Your OTP for login is ${otp}`,
-    });
+// export const sendOTPEmail = async (email, otp) => {
+//   try {
+//     const info = await transporter.sendMail({
+//       from: process.env.EMAIL_USER,
+//       to: email,
+//       subject: "Your OTP Verification Code",
+//       text: `Your OTP for login is ${otp}`,
+//     });
 
-    console.log("Mail sent:", info.messageId);
-  } catch (error) {
-    console.error("Mail error:", error);
-    throw error;
-  }
-};
+//     console.log("Mail sent:", info.messageId);
+//   } catch (error) {
+//     console.error("Mail error:", error);
+//     throw error;
+//   }
+// };

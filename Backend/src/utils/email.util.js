684d93ecf88133623b9271ec830e7f03837fa35f
Akashjain <aj0881871@gmail.com>
2026-06-02 13:54:12 +0530

    adding the otp wokring 2.0

--- Backend/src/utils/email.util.js
+++ Backend/src/utils/email.util.js
@@ -1,8 +1,17 @@
 import nodemailer  from "nodemailer";
-export const transporter = nodemailer.createTransport({
+// export const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false,        // false for port 587
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,        // false for port 587
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,

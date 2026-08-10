96f079fbef39f15c554b83e48552b97313565d78
Akashjain <aj0881871@gmail.com>
2026-08-10 21:18:02 +0530

    fixing the shared name foldser structure file name

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 import SignUp from './pages/signUp';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
